5418a62303c48f8c59e03c384b3e5ca573f4d8a3
Clara Nguyen <idesty@derpg.xyz>
2018-04-24 15:43:04 -0400

    Added Kanji for each day of the week (youbi)

---
+++ scr/print_day.sh
@@ -0,0 +1,31 @@
+DAY=$(date +%u)
+if [[ $# -eq 1 ]]; then
+	DAY=$1
+fi
+DIR="${HOME}/youbi"
+FILEARR=(
+	''
+	'monday.txt'
+	'tuesday.txt'
+	'wednesday.txt'
+	'thursday.txt'
+	'friday.txt'
+	'saturday.txt'
+	'sunday.txt'
+)
+
+NOCOLOUR=$(tput sgr0)
+COLOURS=(
+	$(tput sgr0)
+	$(tput setaf 213)
+	$(tput setaf 196)
+	$(tput setaf 14)
+	$(tput setaf 10)
+	$(tput setaf 11)
+	$(tput setaf 211)
+	$(tput setaf 105)
+)
+
+printf "${COLOURS[$DAY]}"
+cat "${DIR}/${FILEARR[$DAY]}"
+printf "$NOCOLOUR"
